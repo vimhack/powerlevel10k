d530d33b742105b174135b5579cc819510e8f1f0
windvalley <i@sre.im>
2021-10-19 11:59:12 +0800

    refactor: optimize p10k.zsh for current dir color

--- p10k.zsh
+++ p10k.zsh
@@ -215,7 +215,7 @@
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=4
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=3
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=4
